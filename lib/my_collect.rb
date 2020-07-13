ad9fd6683d84d1c1e7a42c40bd2f6f9bafef5720
Ariel Barclay <mrsbarclay101008@gmail.com>
2020-07-13 13:45:00 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
 
+def my_collect(students)
+  i = 0
+  names =[]
+  while i < students.length
+  names << yield(students[i])
+  i += 1
+end
+names
+end
+
+my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]){|name|
+  name.split(" ").first}
+         
+         
 
