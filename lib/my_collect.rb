ca0e3f23cfa9f9cc292d28bc3f85fe5fe689cf01
Cameron Cousins <cameroncousins18@gmail.com>
2020-07-15 18:48:54 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,22 @@
+require "pry" 
+
+def my_collect(collection)
+  first_name = []
+  i = 0  
+  while i < collection.length 
+    first_name << yield(collection[i])
+    i += 1
+  end
+  first_name
+end
 
 
+#collection = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+#my_collect(collection) do |name|
+#  name.split(" ").first
+#end
+
+#collection = ['ruby', 'javascript', 'python', 'objective-c']
+#my_collect(collection) do |lang|
+#  lang.upcase
+#end
