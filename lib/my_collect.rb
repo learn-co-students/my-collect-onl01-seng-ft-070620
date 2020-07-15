ebf32f058e87ac6113e57ed9474f6fd1b32475b3
Jorge Venegas <brangelina01@gmail.com>
2020-07-15 06:02:14 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
+def my_collect(array)
+  x = 0 
+  new_array = []
+  while x < array.length
+    if block_given?
+    new_array << yield(array[x])
+  end 
+    x += 1 
+  end 
+  puts new_array.inspect 
+  new_array
+end 
 
